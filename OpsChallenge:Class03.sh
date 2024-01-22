6755801d9ac395fa640efae472304850a9482294
Cody Blahnik <153008650+Cody354@users.noreply.github.com>
2024-01-22 13:31:07 -0600

    Create OpsChallenge:Class03.sh

---
+++ OpsChallenge:Class03.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script Name:                  test
+# Author:                       Cody Blahnik
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# PRICE OF APPLES
+#One equal sign means assigning a value
+#Two is comparing values
+
+PRICE_PER_APPLE=5
+MyFirstLetters=ABC
+greeting="HELLO WORLLD"
+FILES=`ls`
+WHO=`whoami`
+# Declaration of functions
+
+
+
+# Main
+
+echo $greting
+echo "the price of an apple today is: $PRICE_PER_APPLE"
+echo "The first ten letters of the alphabet are: ${MyFirstLetters}DEFGHIJ"
+echo "This fold contains the ffollowing $FILES"
+echo "The person running this script is $WHO"
+
+
+# End
